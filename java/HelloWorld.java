5a81855fb2b7d37d6f7ae8ae96fd68965534452f
Tony Mao [tmao] <tmao88@outlook.com>
2025-02-13 09:56:45 -0600

    test3 2/13/2025

--- java/HelloWorld.java
+++ java/HelloWorld.java
@@ -1,5 +1,16 @@
 class HelloWorld {
     public static void main(String[] args) {
+        String name = "Tony";
+        if (args.length == 0);
+        t(1400, 70);
         System.out.println("Hello, World!");
     }
+
+    public int t(int a, int b) {
+        if (b == 0)
+            return Integer.MAX_VALUE;
+        else
+            return a / b;
+    }
+
 }
