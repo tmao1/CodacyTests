dc74fc48566dff64c43aceba084c540db24d2080
Tony Mao [tmao] <tmao88@outlook.com>
2025-02-27 18:43:50 -0600

    Tony Test1

--- java/LongestCommonSubsequence.java
+++ java/LongestCommonSubsequence.java
@@ -71,7 +71,7 @@ public class LongestCommonSubsequence {
             return;
         }
 
-        String name = "Tony Test";
+        String name = "Tony Test1";
         if (args.length == 0);
         t(1400, 70);
 
