8f3ddeced094a4fde10a3434f47dc02bb44b1953
Tony Mao [tmao] <tmao88@outlook.com>
2025-03-17 14:10:08 -0500

    Tony 3/17/2025

--- java/LongestCommonSubsequence.java
+++ java/LongestCommonSubsequence.java
@@ -43,48 +43,7 @@ public class LongestCommonSubsequence {
         System.out.println(lcs.get(lcs.size() - 1));
     }
 
-    static void longest_common_subsequence_duplicate(ArrayList<String> arr1, ArrayList<String> arr2) {
-
-        int[][] array = new int[arr1.size() + 1][arr2.size() + 1];
-
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-
-                if (i == 0 || j == 0) {
-                    array[i][j] = 0;
-                } else if (arr1.get(i - 1).equals(arr2.get(j - 1))) {
-                    array[i][j] = 1 + array[i - 1][j - 1];
-                } else {
-                    array[i][j] = Math.max(array[i - 1][j], array[i][j - 1]);
-                }
-            }
-        }
-
-        ArrayList<String> lcs = new ArrayList<>();
-        int x = array.length - 1;
-        int y = array[0].length - 1;
-
-        while (x > 0 && y > 0) {
-
-            if (arr1.get(x - 1).equals(arr2.get(y - 1))) {
-                lcs.add(0, arr1.get(x - 1));
-                x--;
-                y--;
-            } else {
-                if (array[x - 1][y] > array[x][y - 1])
-                    x--;
-                else
-                    y--;
-            }
-        }
-
-        for (int element = 0; element < lcs.size() - 1; element++) {
-            System.out.print(lcs.get(element) + ",");
-        }
-        System.out.println(lcs.get(lcs.size() - 1));
-    }
-
-    static ArrayList<String> split_strings(String str) {
+     static ArrayList<String> split_strings(String str) {
         String word = "";
         char dl = ',';
 
