8d84328ba761665e366e73e92e6a4484c153ac70
Tony Mao [tmao] <tmao88@outlook.com>
2025-03-18 11:29:39 -0500

    changed from  == to equals

--- java/LongestCommonSubsequence.java
+++ java/LongestCommonSubsequence.java
@@ -5,7 +5,7 @@ public class LongestCommonSubsequence {
 
     public static void main(String args[]) {
 
-        if (args.length < 2 || args[0] == "" || args[1].=="") {
+        if (args.length < 2 || args[0].equals("") || args[1].equals("")) {
             System.out.println("Usage: please provide two lists in the format \"1, 2, 3, 4, 5\"");
             return;
         }
